5b0e565b3f8b2b2eedbb2c21add7edb4bc78f852
Chaitanya <100409565+chait-phani@users.noreply.github.com>
2022-03-15 14:52:07 +0530

    changes added

--- forherobj_app/urls.py
+++ forherobj_app/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
 
     path('', views.home, name='home'),
-    path('login', views.login, name='login'),
+    path('login', views.login_fun, name='login'),
     path('signup', views.signup, name='signup'),
     path('logout', views.logout, name='logout'),
 
--- forherobj_app/views.py
+++ forherobj_app/views.py
@@ -3,19 +3,22 @@ from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import check_password
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
 
 import requests
 
-from .decorator import is_authenticated
 from .models import WeatherDetails
 
 
-@is_authenticated
+@login_required(login_url='/login')
 def home(request):
 
+    print('------requetd user-----', request.user)
     data = {}
     if request.method == 'POST':
         get_city = request.POST.get('city')
+
         key = '271d1234d3f497eed5b1d80a07b3fcd1'
         url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}'.format(get_city, key)
         res = requests.get(url).json()
@@ -25,6 +28,7 @@ def home(request):
             try:
                 weatherdetail_obj = WeatherDetails.objects.create(city_name=res['name'], longitude=res['coord']['lon'], latitude=res['coord']['lat'], temperature=celsius, country=res['sys']['country'], weather_type=res['weather'][0]['main'], icon=res['weather'][0]['icon'], user_requested=request.user)
                 weatherdetail_obj.save()
+
             except Exception as e:
                 print('----exception as e---', e)
                 messages.error(request, 'some error internally..')
@@ -35,11 +39,10 @@ def home(request):
             data['icon'] = res['weather'][0]['icon']
         else:
             messages.error(request, 'The entered city not found..')
-    print('------data------', data)
     return render(request, 'forherobj_app/home.html', {'data':data})
 
 
-@is_authenticated
+@login_required(login_url='/login')
 def logout(request):
     del request.session['email']
     return redirect('login')
@@ -69,20 +72,32 @@ def signup(request):
     return render(request, 'forherobj_app/login.html')
 
 
-def login(request):
+def login_fun(request):
     
     if request.method == 'POST':
         email_input = request.POST.get('email_temp')
         pwd = request.POST.get('pass_temp')
 
+        print('-----email input-------',email_input)
+        print('-----pwd input-------',pwd)
         if User.objects.filter(email=email_input).exists():
             user_obj = User.objects.get(email=email_input)
-            if user_obj and user_obj.check_password(pwd) == True:
-                request.session['email'] = user_obj.email
+            
+            user = authenticate(username=user_obj.username, password=pwd)
+            login(request, user)
+            print('-------user obj in login------', user)
+            if user:
+                print('-----inside if user obj and pwd match------')
+            #     request.session['email'] = user_obj.email
+            #     print('-------request session in login----', request.session['email'])
                 messages.success(request, 'You are successfully logged-in.')
+                print('------final print of user------', user_obj.username, user_obj.email)
                 return redirect('/')
             else:
+                print('------pwd mismatch------')
                 messages.error(request, 'Password mismatch..!')
         else:
+            print('------wrong email----------')
             messages.error(request, 'Please check email..!')
     return render(request, 'forherobj_app/login.html')
+
