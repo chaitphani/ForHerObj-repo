51904fce1be75c51b0f810e589008e5c5a7353f3
Chaitanya <100409565+chait-phani@users.noreply.github.com>
2022-09-19 19:36:00 +0530

    added print statement to get the city enetered

--- forherobj_app/views.py
+++ forherobj_app/views.py
@@ -17,6 +17,7 @@ def home(request):
     data = {}
     if request.method == 'POST':
         get_city = request.POST.get('city')
+        print('----get city name-----', get_city)
 
         key = '271d1234d3f497eed5b1d80a07b3fcd1'
         url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}'.format(get_city, key)
